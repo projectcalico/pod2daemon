7d738499c5f72164cc95787178c015fa975dd4d4
Walter Neto <walter@tigera.io>
2021-11-10 00:31:18 -0300

    Introduces BinderOptions
    - Creates "FilterCredential" func that returns a BinderOption

---
+++ binder/binder_option.go
@@ -0,0 +1,31 @@
+// Copyright (c) 2018 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package binder
+
+type BinderOption interface {
+	apply(*binder)
+}
+
+type funcCredentialFilter func(Credentials) error
+
+func (f funcCredentialFilter) apply(b *binder) {
+	b.workloads.credentialFilter = f
+}
+
+// FilterCredential is a function that receives the connection Credential info
+// return a non nil erro to deny the credential.
+func CredentialFilter(f func(Credentials) error) BinderOption {
+	return funcCredentialFilter(f)
+}
